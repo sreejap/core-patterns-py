2fa39e3d54e2f112a73c014782fb87f5a9244bf9
sreejap <sreeja605@gmail.com>
2026-04-17 14:38:44 -0400

    Optimize word lookup using a set
    Replaced wordDict with word_set for optimized lookups.

--- Recursion/WordBreakII.py
+++ Recursion/WordBreakII.py
@@ -2,12 +2,13 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         ans = []
+        word_set = set(wordDict) # look up in lists in o(n), set is o(1)
         def dfs (start,path):
             if start == len (s):
                 ans.append (" ".join(path))
                 return
             
-            for word in wordDict:
+            for word in word_set:
                 if s[start:].startswith(word):
                     path.append(word)
                     dfs (start+len(word),path)
