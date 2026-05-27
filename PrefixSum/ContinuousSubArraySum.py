174532d30fc1002384580770a05b168047b54468
sreejap <sreeja605@gmail.com>
2026-05-27 15:30:58 -0400

    Update ContinuousSubArraySum.py

--- PrefixSum/ContinuousSubArraySum.py
+++ PrefixSum/ContinuousSubArraySum.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/continuous-subarray-sum/editorial/
 # https://leetcode.com/problems/continuous-subarray-sum/solutions/5276981/prefix-sum-hashmap-patterns-7-problems/ 
-# We need Prefix Sum + Hashmap pattern because sliding window is only applicable when we know for sure if the prefixsum is an increasing or decreasing function (i.e. Monotonous in 
-nature)
+# We need Prefix Sum + Hashmap pattern because sliding window is only applicable when we know for sure if the prefixsum 
+# is an increasing or decreasing function (i.e. Monotonous in nature)
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         prefix_mod = 0
